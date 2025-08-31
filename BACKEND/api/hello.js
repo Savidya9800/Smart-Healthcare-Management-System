fd957cfb27d28cbfcabb9b9e2b9f8d5473c22ec7
Savidya Jayalath <146700155+Savidya9800@users.noreply.github.com>
2025-08-31 17:31:08 +0530

    Update comment in hello.js for clarity

--- BACKEND/api/hello.js
+++ BACKEND/api/hello.js
@@ -1,4 +1,4 @@
-// Sample Vercel Serverless Function (moved to BACKEND/api)
+// Sample Vercel Serverless Function
 module.exports = (req, res) => {
   res.status(200).json({ message: "Hello from Vercel Serverless Function in BACKEND/api!" });
 };
